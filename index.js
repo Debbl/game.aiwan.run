231a6841c672fa38914a5dc2c942445f346a3299
Debbl <me@aiwan.run>
2022-11-12 21:04:18 +0800

    feat: add tictactoe

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ fetch(
     const el = document.querySelector(".minesweeper-md");
     el.innerHTML = result;
   });
+
+fetch(
+  "https://raw.githubusercontent.com/Debbl/solidjs-tic-tac-toe/main/README.md"
+)
+  .then((response) => response.text())
+  .then((data) => {
+    const result = md.render(data);
+    const el = document.querySelector(".tictactoe-md");
+    el.innerHTML = result;
+  });

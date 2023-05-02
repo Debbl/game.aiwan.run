61089bcf45633f1df0677cd5fd34d85408398b4f
Debbl <me@aiwan.run>
2023-05-02 21:17:53 +0800

    fix: service worker

--- _serviceWorker.js
+++ _serviceWorker.js
@@ -3,43 +3,40 @@ const AIWAN_GAME_CACHE = "aiwan-game-cache-" + "$VERSION$";
 const cache_url = "$CACHE_URL$";
 
 //  install
-self.addEventListener("install", async () => {
-  const cache = await caches.open(AIWAN_GAME_CACHE);
-  await cache.addAll(cache_url);
-  await self.skipWaiting();
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(AIWAN_GAME_CACHE)
+      .then((cache) => cache.addAll(cache_url))
+      .then(() => self.skipWaiting())
+  );
 });
 
 // activate
-self.addEventListener("activate", async () => {
-  const keys = await caches.keys();
-  keys.forEach(async (key) => {
-    if (key !== AIWAN_GAME_CACHE) {
-      await caches.delete(key);
-    }
-  });
-  await self.clients.claim();
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => {
+        return Promise.all(
+          keys.map((key) => {
+            if (key !== AIWAN_GAME_CACHE) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
 });
 
 // fetch
-self.addEventListener("fetch", async (event) => {
+self.addEventListener("fetch", (event) => {
   console.log(event.request.url);
-  event.respondWith(cacheFirst(event.request));
+  event.respondWith(
+    caches
+      .open(AIWAN_GAME_CACHE)
+      .then((cache) => cache.match(event.request))
+      .then((response) => response || fetch(event.request))
+  );
 });
-
-async function networkFirst(request) {
-  return await fetch(request).catch(async () => {
-    const cache = await caches.open(AIWAN_GAME_CACHE);
-    return cache.match(request);
-  });
-}
-
-async function cacheFirst(request) {
-  const cache = await caches.open(AIWAN_GAME_CACHE);
-  const cached = await cache.match(request);
-  cached &&
-    console.log(
-      "🚀 ~ file: _serviceWorker.js:39 ~ cacheFirst ~ cached:",
-      cached
-    );
-  return cached || (await fetch(request));
-}

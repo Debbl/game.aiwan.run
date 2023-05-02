67cba0362651fc5d3cc0180817f4c8b6a8798e82
Debbl <me@aiwan.run>
2023-05-02 20:38:29 +0800

    fix: service worker register

--- public/serviceWorkerRegister.js
+++ public/serviceWorkerRegister.js
@@ -1,12 +1,14 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./serviceWorker.js")
-    .then((registration) => {
-      console.log("SW Registered!");
-      console.log(registration.scope);
-    })
-    .catch((error) => {
-      console.log("SW Registration Failed!");
-      console.log(error);
-    });
-}
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("./serviceWorker.js")
+      .then((registration) => {
+        console.log("SW Registered!");
+        console.log(registration.scope);
+      })
+      .catch((error) => {
+        console.log("SW Registration Failed!");
+        console.log(error);
+      });
+  }
+});

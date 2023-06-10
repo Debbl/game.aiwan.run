adb30e5276f9de2dad8ca615a0ecd8a87b4474ef
Debbl <me@aiwan.run>
2023-06-10 17:26:25 +0800

    feat: add game of life

--- public/index.js
+++ public/index.js
@@ -1,22 +1,15 @@
 const md = window.markdownit({
   html: true,
 });
-fetch(
-  "https://raw.githubusercontent.com/Debbl/react-minesweeper/main/README.md"
-)
-  .then((response) => response.text())
-  .then((data) => {
-    const result = md.render(data);
-    const el = document.querySelector(".minesweeper-md");
-    el.innerHTML = result;
-  });
 
-fetch(
-  "https://raw.githubusercontent.com/Debbl/solidjs-tic-tac-toe/main/README.md"
-)
-  .then((response) => response.text())
-  .then((data) => {
-    const result = md.render(data);
-    const el = document.querySelector(".tictactoe-md");
-    el.innerHTML = result;
-  });
+const pages = ["react-minesweeper", "solidjs-tic-tac-toe", "game-of-life"];
+
+pages.forEach((p) => {
+  fetch(`https://raw.githubusercontent.com/Debbl/${p}/main/README.md` )
+    .then((response) => response.text())
+    .then((data) => {
+      const result = md.render(data);
+      const el = document.querySelector(`.${p}-md`);
+      el.innerHTML = result;
+    });
+});
